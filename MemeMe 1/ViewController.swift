e59e64ca169099b4cd38f2e99781fc2de9abd7b3
Jennifer Person <jenniferperson@jennifers-macbook-pro.local>
2016-05-08 13:56:34 -0700

    Created pick button, added imageView, and set up so that once a photo is selected it shows up in imageView.

--- MemeMe 1/ViewController.swift
+++ MemeMe 1/ViewController.swift
@@ -8,18 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate {
 
+    @IBOutlet weak var imagePickerView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func pickAnImage(sender: AnyObject) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        presentViewController(imagePicker, animated: true, completion: nil)
+        
 
+    }
+    
+    func imagePickerController(_picker: UIImagePickerController,
+        didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+            
+       if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+        imagePickerView.image = image
+        }
+            
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
 }
 
